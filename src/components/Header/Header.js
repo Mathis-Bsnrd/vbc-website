cee380501a794854a84ca14ba2de8398be180f09
Mathis-Bsnrd <mathisbesnard3@gmail.com>
2024-10-16 15:57:13 +0200

    add logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../logo-vbc.png";
 
 const Header = () => (
   <header className="header">
-    <div className="logo">Venansault Basketball Club</div>
+    <div className="logo">
+      <img src={logo} alt="Logo VBC"></img>
+    </div>
     <nav className="nav">
       <a href="#home">Accueil</a>
-      <a href="#team">Équipe</a>
-      <a href="#results">Résultats</a>
-      <a href="#schedule">Calendrier</a>
+      <a href="#team">Nos équipes</a>
+      <a href="#results">Le club</a>
+      <a href="#schedule">Nos partenaires</a>
       <a href="#contact">Contact</a>
     </nav>
   </header>
