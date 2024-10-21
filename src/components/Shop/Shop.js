2c06e4fe78fc605874c922c9a8bc97343ff671e1
Mathis-Bsnrd <mathisbesnard3@gmail.com>
2024-10-21 14:33:41 +0200

    update shop button

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,13 +1,42 @@
 import React from "react";
 import "./Shop.css";
 import { Button } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 const Shop = () => {
+  const handleOpenLink = () => {
+    window.open(
+      "https://www.helloasso.com/associations/venansault-basket-club/boutiques/boutique-vbc-24-25",
+      "_blank"
+    );
+  };
+
   return (
     <div>
-      <img src="https://cdn.helloasso.com/img/photos/shops/croppedimage-dfeaca65ee3b4cb3949589d09d9fd914.png?resize=fill:1920:250" />
-      <img src="https://venansaultbasket.fr/wp-content/uploads/2024/07/Boutique-BASKET-CLUB-VENANSAULT-2-pdf.jpg" />
-      <Button>Commander</Button>
+      <div>
+        <h1>Boutique saison 2024 - 2025</h1>
+        <img
+          className="shop-image"
+          src="https://venansaultbasket.fr/wp-content/uploads/2024/07/Boutique-BASKET-CLUB-VENANSAULT-2-pdf.jpg"
+        />
+      </div>
+      <div className="shop-button">
+        <Button
+          variant="contained"
+          onClick={handleOpenLink}
+          endIcon={<AddShoppingCartIcon />}
+          sx={{
+            backgroundColor: "#A5120D",
+            margin: "40px",
+            width: "250px",
+            height: "50px",
+            fontSize: "22px",
+            textTransform: "none",
+          }}
+        >
+          Commander
+        </Button>
+      </div>
     </div>
   );
 };
