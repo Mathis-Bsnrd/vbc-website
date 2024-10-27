3c40893b82e8c32b789a62e1b2dd5efeacdbac07
Mathis-Bsnrd <mathisbesnard3@gmail.com>
2024-10-27 16:53:36 +0100

    add date for newsItems

--- src/components/NewsDetail/NewsDetail.js
+++ src/components/NewsDetail/NewsDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Typography from "@mui/material/Typography";
+import { Box } from "@mui/material";
 
 const NewsDetail = () => {
   const location = useLocation();
@@ -11,12 +12,28 @@ const NewsDetail = () => {
   }
 
   return (
-    <div>
-      <Typography variant="h4" component="h2">
+    <Box sx={{ padding: 2 }}>
+      <Typography variant="h4" component="h2" gutterBottom>
         {news.title}
       </Typography>
+      <Typography variant="h4" component="h4" gutterBottom>
+        {news.date}
+      </Typography>
+      <Box
+        component="img"
+        src={news.image}
+        alt={news.title}
+        sx={{
+          width: "100%",
+          height: "auto",
+          maxHeight: "300px",
+          objectFit: "contain",
+          borderRadius: 2,
+          mb: 2,
+        }}
+      />
       <Typography variant="body1">{news.description}</Typography>
-    </div>
+    </Box>
   );
 };
 
